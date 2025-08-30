803130e0c1381355626c22ceef1d0aa743cb7b17
Parikshit Sankhyan <parikshitsankhyan0024@gmail.com>
2025-08-30 23:05:40 +0530

    mongo atlas

--- Backend/config/mongoose-connection.js
+++ Backend/config/mongoose-connection.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const dbgr = require('debug')('development:mongoose');
 
-mongoose.connect(`${config.get('MONGODB_URL')}/fullii`)
+const dbURI = process.env.MONGODB_URL || config.get('MONGODB_URL');
+
+mongoose.connect(`${dbURI}/fullii`)
     .then(() => {
-        dbgr('Connected to MongoDB');
+        dbgr('✅ Connected to MongoDB');
     })
     .catch((err) => {
-        dbgr('Error connecting to MongoDB:', err.message);
+        dbgr('❌ Error connecting to MongoDB:', err.message);
         process.exit(1);
     });
 
